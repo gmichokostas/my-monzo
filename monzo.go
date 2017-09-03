1d7261fdcf3cb5d06bf2203c63d68e56fe77f36a
George Michokostas <michokostas@gmail.com>
2017-09-03 16:01:34 +0200

    create Transaction getters, use struct embedding, function renaming

--- monzo.go
+++ monzo.go
@@ -19,3 +19,22 @@ type Data struct {
 type Merchant struct {
 	Name string
 }
+
+func (t Transaction) description() string {
+	return t.Data.Description
+}
+
+func (t Transaction) amount() float64 {
+	return float64(t.Data.Amount) / float64(100)
+}
+
+func (t Transaction) currency() string {
+	return t.Data.Currency
+}
+func (t Transaction) category() string {
+	return t.Data.Category
+}
+
+func (t Transaction) merchantName() string {
+	return t.Data.Merchant.Name
+}
